0abe89497ee3258518676d3de7041b3bd8a259be
Isaac Avilez <isaac1180@aol.com>
2020-02-17 14:26:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function scuberGreetingForFeet(){
   // Write your code here!
   let customer;
-  const ride = 300 
+  const ride = 2000 
   
-  if (ride =< 400){
+  if (ride =< 400) {
     let customer = true; 
     ("Here's your free sample!");
   }
-  else if () {
+  else if (ride ) {
     
   }
 }
