594e332f78070ba4dd047a87b800cfc036bd3b07
Isaac Avilez <isaac1180@aol.com>
2020-02-17 14:29:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ function scuberGreetingForFeet(){
   if (ride <= 400) {
     let customer = true;
     ("Here's your free sample!");
+    
   } else if (ride > 2000) {
     ("Charging $30 for the ride");
+    
   } else (ride > 2500) {
     customer = false;
   }
